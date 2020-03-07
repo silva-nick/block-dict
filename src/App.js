a8c8af8d9e55356959fbdf5fa594a37feaf33faa
Nick Silva <silva.d.nicholas@gmail.com>
2020-03-07 12:00:44 -0600

    start page layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputWords: [],
+      outputWords: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="Search">
+          <text class="title">Translate a word </text>
+          <input type="text" placeholder="Search.."></input>
+        </div>
+        <div className="InputBlocks">
+          <text>Testing</text>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
