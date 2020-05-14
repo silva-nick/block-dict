c0ba64d17949fcd87122ac56eab839e7c571e214
Nick Silva <silva.d.nicholas@gmail.com>
2020-05-13 21:08:51 -0500

    fix response bug

--- block-server/http-server/server.js
+++ block-server/http-server/server.js
@@ -6,7 +6,6 @@ http
   .createServer((request, response) => {
     console.log(request.method);
     console.log(request.url);
-    console.log(request.url.indexOf("api/dictionary") > -1);
     let body = [];
     if (
       request.method === "POST" &&
@@ -25,28 +24,29 @@ http
           // respond:
           words = querystring.parse(body);
           console.log(body);
-          console.log(words);
-          for (word in words) {
-            let def = dict.filter((obj) => {
-              return obj.simplified === word;
+          for (wordIndex in words) {
+            let def = dict.find((obj) => {
+              return obj.simplified === words[wordIndex];
             });
             if (def !== undefined) {
               delete def.traditional;
-              response.write(querystring.stringify(def));
+              console.log(querystring.stringify(def));
+              response.write(querystring.stringify(def) + "}");
             }
-            for (word2 in words) {
-              c1 = word + word2;
-              c2 = word2 + word;
+            for (word2Index in words) {
+              c1 = words[wordIndex] + words[word2Index];
+              c2 = words[word2Index] + words[wordIndex];
               def = dict.find((obj) => {
                 return obj.simplified === c1 || obj.simplified === c2;
               });
               if (def !== undefined) {
                 delete def.traditional;
                 console.log(querystring.stringify(def));
-                response.write(querystring.stringify(def));
+                response.write(querystring.stringify(def) + "}");
               }
             }
           }
+          response.end();
         });
     } else {
       response.statusCode = 404;
