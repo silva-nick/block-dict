c1a55e3ba50863f76040e06de169e7b60be8a41e
Nick Silva <silva.d.nicholas@gmail.com>
2020-05-13 21:27:33 -0500

    remove express server and mongodb code; finish transition to http

--- block-server/routes/word-router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const WordController = require("./word-ctrl");
-
-router.get("/word/:id", WordController.getWordById);
-router.get("/char/:char", WordController.getCharacterByCharacter);
-
-module.exports = router;
